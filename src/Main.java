2e384548421df5efce92ef87e6a07302bee7730e
Carlos Rodríguez <crodrui1905@ieszaidinvergeles.org>
2023-12-08 15:41:28 +0100

    Prueba de entornos

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.apache.commons.math3.random.RandomDataGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        // Configura el generador de números aleatorios
+        RandomDataGenerator randomDataGenerator = new RandomDataGenerator();
+
+        // Genera y muestra 10 números aleatorios entre 1 y 100
+        System.out.println("10 números aleatorios entre 1 y 100:");
+        for (int i = 0; i < 10; i++) {
+            int randomNumber = randomDataGenerator.nextInt(1, 100);
+            System.out.println(randomNumber);
+        }
+    }
+}
